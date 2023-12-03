6028d0f5f1a5c6cae01df132ee8216798e16f712
Gopesh Janardhanan <68292507+GamePython-web@users.noreply.github.com>
2023-12-04 00:03:26 +0400

    Color Picker v1.6

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ let container_1 = document.querySelector("#container-1 p")
 let container_2 = document.querySelector("#container-2 p")
 let container_3 = document.querySelector("#container-3 p")
 let container_4 = document.querySelector("#container-4 p")
+const copy_1 = document.querySelector("#copy-1")
+const copy_2 = document.querySelector("#copy-2")
+const copy_3 = document.querySelector("#copy-3")
+const copy_4 = document.querySelector("#copy-4")
 
 
 function container1(){
@@ -38,6 +42,25 @@ function renderColor(){
     container4()
 
 }
+
+function copy1(){
+    navigator.clipboard.writeText(container_1.innerText)
+    console.log(container_1.innerText)
+}
+function copy2(){
+    navigator.clipboard.writeText(container_2.innerText)
+    console.log(container_2.innerText)
+}
+function copy3(){
+    navigator.clipboard.writeText(container_3.innerText)
+    console.log(container_3.innerText)
+}
+function copy4(){
+    navigator.clipboard.writeText(container_4.innerText)
+    console.log(container_4.innerText)
+}
+
+
 renderColor()
 container_1.addEventListener("click", container1)
 container_2.addEventListener("click", container2)
@@ -49,3 +72,7 @@ window.addEventListener('keydown',function(e){
         renderColor()
     }
 })
+copy_1.addEventListener('click',copy1)
+copy_2.addEventListener('click',copy2)
+copy_3.addEventListener('click',copy3)
+copy_4.addEventListener('click',copy4)
