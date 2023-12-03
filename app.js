019fb2ff6b2b91b5a49b906a886e74dd24e034f8
Gopesh Janardhanan <68292507+GamePython-web@users.noreply.github.com>
2023-12-03 11:53:25 +0400

    ColorPicker v1

---
+++ app.js
@@ -0,0 +1,36 @@
+
+let color = document.body.style.backgroundColor = rgb(0, 179, 255);
+
+function container1(){
+    let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    let container1 = document.getElementById("container1")
+    container1.style.backgroundColor = randomColor
+    console.log(randomColor)
+}
+
+function container2(){
+    // console.log("Clicked")
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    let container1 = document.getElementById("container2")
+    container1.style.backgroundColor = "#" + randomColor
+    console.log(randomColor)
+
+}
+
+function container3(){
+    // console.log("Clicked")
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    let container1 = document.getElementById("container3")
+    container1.style.backgroundColor = "#" + randomColor
+    console.log(randomColor)
+
+}
+
+function container4(){
+    // console.log("Clicked")
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    let container1 = document.getElementById("container4")
+    container1.style.backgroundColor = "#" + randomColor
+    console.log(randomColor)
+
+}
