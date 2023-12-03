a2cc5cd1e9760c929e3ceed569979d8ec6e16562
Gopesh Janardhanan <68292507+GamePython-web@users.noreply.github.com>
2023-12-03 23:21:27 +0400

    Color Picker v1.5
    Added space bar randomizer

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-// let color = document.body.style.backgroundColor = rgb(0, 179, 255);
 let container_1 = document.querySelector("#container-1 p")
 let container_2 = document.querySelector("#container-2 p")
 let container_3 = document.querySelector("#container-3 p")
@@ -13,7 +12,6 @@ function container1(){
 }
 
 function container2(){
-    // console.log("Clicked")
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
     container_2.style.backgroundColor = "#" + randomColor
     container_2.innerText = randomColor
@@ -21,7 +19,6 @@ function container2(){
 }
 
 function container3(){
-    // console.log("Clicked")
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
     container_3.style.backgroundColor = "#" + randomColor
     container_3.innerText = randomColor
@@ -29,18 +26,22 @@ function container3(){
 }
 
 function container4(){
-    // console.log("Clicked")
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
     container_4.style.backgroundColor = "#" + randomColor
     container_4.innerText = randomColor
 
 }
-container1()
-container2()
-container3()
-container4()
-container_1.addEventListener("click", container1)
-container_2.addEventListener("click", container2)
-container_3.addEventListener("click", container3)
-container_4.addEventListener("click", container4)
+function renderColor(){
+    container1()
+    container2()
+    container3()
+    container4()
 
+}
+renderColor()
+
+window.addEventListener('keydown',function(e){
+    if (e.key == " "){
+        renderColor()
+    }
+})
